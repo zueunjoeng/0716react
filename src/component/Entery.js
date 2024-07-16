88f9599e9f4123d1179f9bbb7e3c66dab0efd36a
zueunjoeng <judms0314@gmail.com>
2024-07-16 12:06:25 +0900

    map -return

--- src/component/Entery.js
+++ src/component/Entery.js
@@ -1,21 +1,23 @@
 import React, {useEffect, useState} from 'react'
 
 function Entery() {
-    let count = 0;
-    //일반변수 상태변수값을 받아서 출력하려면 return 출력식에서 처리하기
-    const [bgcolor, setBg] = useState('red'); //랜더링 
-    //스타일 랜더링 classList 관련 명령은 useEffect에서 처리하기 딱 한번
+   const bgcolor = ['red', 'green', 'blue']
+
+   useEffect(()=>{
+    // document.querySelectorAll('li').forEach()
+    //li는 태그. forEach는 어레이도 받고 변수도 받고?
+   })
     return (
         <div>
-            <button onClick={()=>{
-                document.body.style.backgroundColor = 'blue'
-            }}>파</button>
-            <button onClick={()=>{
-                document.body.style.backgroundColor = 'yellow'
-            }}>노</button>
-            <button onClick={()=>{
-                document.body.style.backgroundColor = 'green'
-            }}>초</button>
+            {
+                bgcolor.map((e, i)=>{
+                    return(
+                        <button onClick={()=>{
+                            document.body.style.backgroundColor = e;
+                        }}>{e}</button>
+                    )
+                })
+            }
             
         </div>        
     )
