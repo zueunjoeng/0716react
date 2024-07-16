f9babbb3bde16376150cfd90373ef2efc2f3dd47
zueunjoeng <judms0314@gmail.com>
2024-07-16 11:37:30 +0900

    count의 상태변수값을 대입해서 출력 -버튼클릭시 증가

--- src/component/Entery.js
+++ src/component/Entery.js
@@ -1,20 +1,21 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 
 function Entery() {
     let count = 0;
-    const[num, setNum] = useState(0);
+    //일반변수는 여전히 상태변수를 받집못해서.. 
+    //useRef가 등장하게 된다. 
+    const[num, setNum] = useState(0); //랜더링
     return (
         <div>
             <button onClick={
                         ()=>{ 
-                            count = count + 1; 
-                            console.log(count);
-                            setNum(num + 1); 
-                            //num=10;(웹에서) 리액트에서는 setNum(10);로 대입해주는거야
-                            //상태변수의 값을 변경하는 상태함수
+                        
+                           setNum(num + 1); //상태변수의 값을 변경하는 상태함수
+                            count = num; //화살표함수 내에서 저장안됨
+                            
                         }
                        }>일반변수 count 하나씩 증가</button>
-            <h1>나는 일반변수 count : { `${count} ${num}` } </h1>
+            <h1>나는 일반변수 count의 상태변수값을 대입해서 출력 : { `${count = num}` } </h1>
         </div>        
     )
 }
