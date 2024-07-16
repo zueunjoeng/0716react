955048a6fbfb6dd0222b07285caaa492a3f33df2
zueunjoeng <judms0314@gmail.com>
2024-07-16 11:41:55 +0900

    useEffect

--- src/component/Entery.js
+++ src/component/Entery.js
@@ -5,16 +5,18 @@ function Entery() {
     //일반변수는 여전히 상태변수를 받집못해서.. 
     //useRef가 등장하게 된다. 
     const[num, setNum] = useState(0); //랜더링
+    useEffect(()=>{
+        document.body.style.backgroundColor = 'red'
+
+    }, []);
+    useEffect(()=>{
+        document.querySelector('button').addEventListener('click', ()=>{
+            setNum(num +1);
+        })
+    }, [num]); //랜더링이 끝나고 나서 html태그로 이해
     return (
         <div>
-            <button onClick={
-                        ()=>{ 
-                        
-                           setNum(num + 1); //상태변수의 값을 변경하는 상태함수
-                            count = num; //화살표함수 내에서 저장안됨
-                            
-                        }
-                       }>일반변수 count 하나씩 증가</button>
+            <button>일반변수 count 하나씩 증가</button>
             <h1>나는 일반변수 count의 상태변수값을 대입해서 출력 : { `${count = num}` } </h1>
         </div>        
     )
