5f6e6732505e498de33bc582fe88902402b95051
zueunjoeng <judms0314@gmail.com>
2024-07-16 11:49:54 +0900

    스타일 랜더링

--- src/component/Entery.js
+++ src/component/Entery.js
@@ -2,22 +2,21 @@ import React, {useEffect, useState} from 'react'
 
 function Entery() {
     let count = 0;
-    //일반변수는 여전히 상태변수를 받집못해서.. 
-    //useRef가 등장하게 된다. 
-    const[num, setNum] = useState(0); //랜더링
-    useEffect(()=>{
-        document.body.style.backgroundColor = 'red'
-
-    }, []);
-    useEffect(()=>{
-        document.querySelector('button').addEventListener('click', ()=>{
-            setNum(num +1);
-        })
-    }, [num]); //랜더링이 끝나고 나서 html태그로 이해
+    //일반변수 상태변수값을 받아서 출력하려면 return 출력식에서 처리하기
+    const [bgcolor, setBg] = useState('red'); //랜더링 
+    //스타일 랜더링 classList 관련 명령은 useEffect에서 처리하기 딱 한번
     return (
         <div>
-            <button>일반변수 count 하나씩 증가</button>
-            <h1>나는 일반변수 count의 상태변수값을 대입해서 출력 : { `${count = num}` } </h1>
+            <button onClick={()=>{
+                document.body.style.backgroundColor = 'blue'
+            }}>파</button>
+            <button onClick={()=>{
+                document.body.style.backgroundColor = 'yellow'
+            }}>노</button>
+            <button onClick={()=>{
+                document.body.style.backgroundColor = 'green'
+            }}>초</button>
+            
         </div>        
     )
 }
