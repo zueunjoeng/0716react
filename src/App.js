6a611cbb7f1e6503beb5d999247dcc006c9dcf5f
zueunjoeng <judms0314@gmail.com>
2024-07-16 10:44:28 +0900

    a는 완전히 다시 랜더링을 하기때문에 로고, 외부연동 외 아껴쓰기

--- src/App.js
+++ src/App.js
@@ -3,19 +3,31 @@ import {  Link, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Board from './component/Board';
 import About from './component/About';
+import Entery from './component/Entery';
 
 function App() {
   return (
   
       <div className="App">
        <div>
+        <a href='/'>로고</a> 
        <Link to='/about'>회사소개</Link>
         <Link to='/board'>공지사항</Link>
+        
        </div>
        <Routes>
+       <Route path='/' element={<Entery />}></Route>
         <Route path='/about' element={<About />}></Route>
         <Route path='/board' element={<Board />}></Route>
        </Routes>
+       
+       <aside>
+        저는 퀵입니다.
+       </aside>
+
+       <footer>
+        저는 하단입니다.
+       </footer>
       </div>
 
   );
---
+++ src/component/Entery.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Entery() {
+    return (
+        <h1>나는 첫페이지 컨텐츠입니다.</h1>
+    )
+}
+
+export default Entery
